7abbab40d591ae1d58174e0e938853ec9a807fc7
JC <hellrayser@gmail.com>
2019-12-06 18:11:42 +0100

    Cambios en la descripción
    descripción duplicada suprimida

--- resources/views/component/detailG.blade.php
+++ resources/views/component/detailG.blade.php
@@ -7,7 +7,7 @@
                 {{-- // mostramos mensaje --}}
                 @include('includes.message')
 
-                <div class="card pub_image_solo pub_image_detail">
+                <div class="card pub_image_solo pub_image_detail_solo">
                     <div class="card-header">
                         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -21,18 +21,15 @@
                     </div>
 
                     <div class="card-body">
-                        <div class="image-container image-detail">
-                            <img src="{{ route('component.file', ['filename' => $component->image_path]) }}" alt="">
+                        <div class="container_solo">
+                            <img class="img-fluid_solo" src="{{ route('component.file', ['filename' => $component->image_path]) }}" alt="">
                         </div>
 
-                        <div class="description">
+                        <div class="name_component">
                             <p>
                                 <strong>{{ $component->name }}</strong>
                             </p>
 
-                            <p>
-                                {{ $component->description }}
-                            </p>
                         </div>
                         <div class="likes">
                             {{--           Comprobar si el usuario le dio like a la imagen--}}
@@ -75,7 +72,7 @@
 
                         {{-- // descripción --}}
                         <div class="clearfix"></div>
-                        <div class="comments">
+                        <div class="description">
                             <h2>Descripción</h2>
                             <hr>
                             <div class="comment">
