570f5333334cc88ecf38231574ee338f8a694dbb
Fernando David Gómez Ortega <ferdgomez@hotmail.es>
2019-12-29 19:28:28 +0100

    espacio antes del nick

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -108,7 +108,7 @@
 
                                     <li class="nav dropdown float-right">
                                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            <strong>Bienvenido/a {{ Auth::user()->nick }}</strong> <span class="caret"></span>
+                                            <strong>Bienvenido/a &nbsp; {{ Auth::user()->nick }}</strong> <span class="caret"></span>
                                         </a>
 
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -194,7 +194,7 @@
 
                             <li class="nav dropdown float-right">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <strong>Bienvenido/a {{ Auth::user()->nick }}</strong> <span class="caret"></span>
+                                    <strong>Bienvenido/a &nbsp; {{ Auth::user()->nick }}</strong> <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
